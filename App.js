00a825b7cbf7880a51e33f936390cc21747d5642
roshkovskypetr <roshkovskypetr@gmail.com>
2019-09-30 13:03:30 +0300

    Add header, first screen

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header'
+import StartGameScreen from './screens/startGameScreen'
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title={'GUESS a NUMBER'}/>
+      <StartGameScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
